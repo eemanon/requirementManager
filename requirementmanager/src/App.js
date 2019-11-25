c5afa48ca77cbbd71cb565b051de4ed98708e8ca
Your Name <you@example.com>
2019-11-25 18:17:14 +0100

    added working app example on how to use requirementcard component

--- requirementmanager/src/App.js
+++ requirementmanager/src/App.js
@@ -13,23 +13,47 @@ const useStyles = {
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <AppBarManager></AppBarManager>
-     <List style={useStyles.List} component="nav" aria-label="main mailbox folders">
-        <ListItem>
-            <RequirementCard category="Behaviour" subcategory="Non-Functional" color="red" link="https://github.com/eemanon/requirementManager/network" content="Note that the three doors (resp. gears) are controlled simultaneously by the same electro-valve." />
-        </ListItem>
-        <ListItem>
-        <RequirementCard category="Role" subcategory="Role" color="green" link="https://mi-git.univ-tlse2.fr/ECb/LGS/blob/master/architecture.txt#L44" content="This software is in charge of controlling gears and doors, of detecting anomalies, and of informing the pilot about the global state of the system and anomalies (if any)." />
-        </ListItem>
-        <ListItem>
-          <RequirementCard />
-        </ListItem>
-      </List>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {requirements: [], relations: [], stack: []};
+    this.getChildID = this.getChildID.bind(this)
+  }
+  getChildID(dataFromChild) {
+    //if chosen column is already present, remove items from stack
+    if(dataFromChild[1]<this.state.stack.length){
+      //remove all items until we are back on this position...
+        this.setState(state => {
+          const stack = state.stack.slice(0,dataFromChild[1]);
+          return {
+            requirements: state.requirements,
+            relations: state.relations,
+            stack
+          };
+        });
+    }
+    this.setState(state => {
+      const stack = state.stack.concat(dataFromChild[0]);
+      return {
+        requirements: state.requirements,
+        relations: state.relations,
+        stack
+      };
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+      <List style={useStyles.List} component="nav" aria-label="main mailbox folders">
+          <ListItem>
+              <RequirementCard relationsymbol="&#8838;" stack={this.state.stack} returnID={this.getChildID} cardid="0" columnid="0" category="Behaviour" subcategory="Non-Functional" color="red" link="https://github.com/eemanon/requirementManager/network" content="Note that the three doors (resp. gears) are controlled simultaneously by the same electro-valve." />
+          </ListItem>
+          <ListItem>
+              <RequirementCard relationsymbol="&#8838;" stack={this.state.stack} returnID={this.getChildID} cardid="1" columnid="0" category="Behaviour" subcategory="Non-Functional" color="red" link="https://github.com/eemanon/requirementManager/network" content="Note that the three doors (resp. gears) are controlled simultaneously by the same electro-valve." />
+          </ListItem>
+        </List>
+      </div>
+    );
+  }
 }
-
 export default App;
