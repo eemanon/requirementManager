648a289b673d5e0a62bc152937e647ba7742b3e3
Your Name <you@example.com>
2019-11-26 12:12:30 +0100

    added explicative image

---
+++ requirementmanager/requirementmanager/precache-manifest.54d205d2734656e721673afbf431dc6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d408ad3de252d0498192eef6055a66",
+    "url": "/~mamadou.mendy/requirementmanager/index.html"
+  },
+  {
+    "revision": "2c439f500ce6bd63ab32",
+    "url": "/~mamadou.mendy/requirementmanager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b071ea969d6688526d84",
+    "url": "/~mamadou.mendy/requirementmanager/static/js/2.53e7a24c.chunk.js"
+  },
+  {
+    "revision": "2c439f500ce6bd63ab32",
+    "url": "/~mamadou.mendy/requirementmanager/static/js/main.b760c6ff.chunk.js"
+  },
+  {
+    "revision": "ebf0c85ca1a90401e72a",
+    "url": "/~mamadou.mendy/requirementmanager/static/js/runtime-main.c6da07b8.js"
+  }
+]);
